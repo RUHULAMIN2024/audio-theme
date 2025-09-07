5ca81850a001653c55d9213177506de24aded5d6
RUHUL AMIN <ruhulamin15jun1999@gmail.com>
2025-09-07 09:58:49 +0600

    render and view are updated

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,5 +1,3 @@
-const iconColor = '#4527a4';
-
 
 export const verticalLineIcon = <svg xmlns='http://www.w3.org/2000/svg' width={24} height={24} viewBox='0 0 14.707 14.707'>
 	<rect x='6.275' y='0' width='2.158' height='14.707' />
